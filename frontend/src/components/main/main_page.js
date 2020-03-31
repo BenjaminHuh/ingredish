81362ab63bcd6645990d809180d6b1ac0abf01ec
Nandhu <knandhu.27@gmail.com>
2020-03-31 00:51:10 -0700

    retain search results

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './main.scss';
+import background from "./background_video.mp4";
+import NavBarContainer from "./../nav/navbar_container";
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavBarContainer/>
+                <video autoPlay muted loop className="background_video">
+                    <source src={background} type="video/mp4" />
+                </video>
+
+                <div className="main">
+                    <div>
+                        <footer className="footer">&copy; 2020 ingredish | <a href="https://github.com/benjaminhuh/ingredish#group-members" target="_blank">Meet the Devs</a></footer>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default MainPage;
---
+++ frontend/src/components/search/saved_search.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { faThList } from "@fortawesome/free-solid-svg-icons";
+import "./saved_search_page.scss";
+import background from "./background_video.mp4";
+
+
+class SavedSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      savedTerm: this.props.match.params.savedTerm
+    }
+    this.saveRecipe = this.saveRecipe.bind(this);
+  }
+
+  componentDidMount() {
+    let terms = [];
+    if (this.state.savedTerm) {
+      terms = this.state.savedTerm.split(',');
+      this.props.fetchRecipes(terms);
+    }
+   
+  }
+
+  saveRecipe(recipeId) {
+    this.props.saveRecipe(this.props.currentUser, recipeId);
+    this.showNotification();
+  }
+
+  showNotification() {
+    document.getElementById("note").style.display = "block";
+    setTimeout(function () {
+      document.getElementById("note").style.display = "none";
+    }, 1000);
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <video autoPlay muted loop className="background_video">
+          <source src={background} type="video/mp4" />
+        </video>
+        <div id="note">Recipe Saved Successfully</div>
+        <div id="no-recipes">
+          Sorry, no recipes match all the ingredients.
+          <br /> Try with fewer ingredients.
+        </div>
+        <br />
+        <br />
+        {this.state.savedTerm ?
+          <div className="backtosearch-box">
+            <nav className="backtosearch">
+              <Link
+                className="backtosearch-text"
+                to={{ pathname: "/search" }}
+              >
+                Search with different Ingredients
+          </Link>
+
+            </nav>
+          </div> : null}
+        <div className="recipes">
+        <ul>
+          {this.props.recipes 
+            ? this.props.recipes.map((recipe, idx) => (
+              <div
+                className="searched_recipe_items"
+                key={`recipe-${idx}`}
+              >
+                <Link className="recipeImg" to={`/recipe/${recipe._id}`}>
+                  <img
+                    src={recipe.image_url}
+                    className="recipeimg"
+                    alt="recipe"
+                  />
+                </Link>
+
+                <div className="recipeinfo">
+                  <Link
+                    className="recipe-info-link"
+                    to={`/recipe/${recipe._id}`}
+                  >
+                    <h1>{recipe.name}</h1>
+                    {recipe.keywords.map((ing, id) => (
+                      <li key={id}>{ing}</li>
+                    ))}
+                  </Link>
+                  <button
+                    id={recipe.id}
+                    type="button"
+                    onClick={() => this.saveRecipe(recipe._id)}
+                  >
+                    {this.props.savedRecipes.includes(recipe._id)
+                      ? "Saved"
+                      : "Save"}
+                  </button>
+
+                </div>
+              </div>
+            ))
+            : null}
+          </ul>
+          </div>
+      
+     
+      </div>
+    );
+  }
+     
+}
+
+export default withRouter(SavedSearch);
